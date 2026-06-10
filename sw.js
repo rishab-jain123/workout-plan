b332363cd0171902d7c2f4602148fe076310cc58
Rishab Jain <rishab_jain@college.harvard.edu>
2026-06-10 13:59:21 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'papa-v3';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return;
+  e.respondWith(
+    caches.open(CACHE).then(async cache => {
+      const hit = await cache.match(e.request);
+      const fetched = fetch(e.request).then(res => {
+        if (res.ok) cache.put(e.request, res.clone());
+        return res;
+      }).catch(() => hit);
+      return hit || fetched;
+    })
+  );
+});
